cd1895277d7f35126104cf1131909ebc6555ffe4
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2023-09-03 13:19:25 -0300

    Criar o banco de dados com Migrations e configurar conexão com banco de dados

--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -1,15 +1,22 @@
+//using ControleFinanceiro.API.Extensions;
+//using ControleFinanceiro.API.Validacoes;
+//using ControleFinanceiro.API.ViewModels;
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
+//using ControleFinanceiro.DAL.Interfaces;
+//using ControleFinanceiro.DAL.Repositorios;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ControleFinanceiro.API
 {
@@ -25,6 +32,18 @@ namespace ControleFinanceiro.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // O primeiro componente a ser configurado é o nosso contexto especificando qual é a classe de contexto
+            // que estamos utilizando e onde está a conexão que levará ao banco de dados
+            // UseSqlServer indica que estou usando o SQL Server
+            // GetConnectionString indica o nome do banco de dados e qual é a instãncia
+            // ConexaoBD esta informado no appSettings.json
+            // Qual banco que vamos usar, nesse caso SQL Server e a string de conexão
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
+
+            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+
+            services.AddCors();
+
             services.AddControllers();
         }
 
