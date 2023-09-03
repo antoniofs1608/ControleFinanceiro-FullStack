15a9bd58a6ef82b5ea39936837b18a9da6d55de7
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2023-09-03 12:03:20 -0300

    Adicionado as Models (BLL = busines logic layer, ou seja, camada lógica de negócios). Adicionar também os packages utilizados nos projetos

---
+++ ControleFinanceiro.BLL/Models/Cartao.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ControleFinanceiro.BLL.Models
+{
+    public class Cartao
+    {
+        public int CartaoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Bandeira { get; set; }
+
+        public string Numero { get; set; }
+
+        public double Limite { get; set; }
+
+        // A FK UsuarioId é do tipo string porque para gerenciamento de usuários vamos
+        // utilizar o Identity que já vem com classes prontas e seus atributos pré 
+        // definidos e lá a chave primária do usuário é uma string, então aqui tem que
+        // ser string também
+        public string UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        // Essa linha abaixo evita desperdiço de memória
+        public virtual ICollection<Despesa> Despesas { get; set; }
+    }
+}
+
